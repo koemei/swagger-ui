2249e7ec5d9f70a6b4585fb91fd38ca795acaa68
Evgeny Bogdanov <evgeny.bogdanov@gmail.com>
2015-05-20 15:44:18 -0700

    customize swagger for koemei

--- gulpfile.js
+++ gulpfile.js
@@ -90,6 +90,7 @@ gulp.task('less', ['clean'], function() {
 
   return gulp
     .src([
+      './src/main/less/koemei.less',
       './src/main/less/screen.less',
       './src/main/less/print.less',
       './src/main/less/reset.less'
